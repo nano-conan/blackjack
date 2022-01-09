40dbb9f3def6cabd4c15c7f773d4c1113512b2e7
Conan <conanjay@icloud.com>
2022-01-09 17:16:18 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use rand::Rng;
+
+#[derive(Copy, Clone)]
+// Why am i deriving here
+enum Suit{
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades,
+    NoCard
+}
+
+
+
+fn main() {
+    println!("Blackjack!");
+
+    //  What will I need for data,
+    //
+    //  The numbers of cards in hand    this could be u32
+    //  the value of cards in hand      this could be u32
+    //
+    //  the suit of cards in hand       this could be enum or an interpreted u32
+    //  the running total               this could be u32
+    //
+    //  the number of cards in dealer hand
+    //  the value of cards in dealer
+    //  the suit of cards in dealer
+    //
+    //
+    //  TODO: add multiple decks
+    //  TODO: check for blackjack
+    //
+
+    let number_of_decks = 1;
+    //this could be in a config file
+    const MAX_CARDS:usize = 5;
+
+    let mut player_cards_in_hand: u32 = 0;
+    let mut player_hand_value = 0;
+    let mut player_suits = [Suit::NoCard; MAX_CARDS];
+    
+    let card = draw_card(number_of_decks, &mut player_cards_in_hand, &mut player_hand_value, &mut player_suits);
+
+    //put this stuff in the function
+    //
+    //Should i? ya i should but use another function to show what im doing
+
+
+
+
+    println!("Hello here are your arrays: player_cards_hand: {}", player_cards_in_hand);
+    println!("Hello here are your arrays: player_hand_value: {}", player_hand_value);
+    println!("Hello here are your arrays: player_suits: {}", player_suits);
+}
+
+fn draw_card(number_of_decks: u32, player_cards_in_hand: &mut u32, player_hand_value: &mut u32, player_suits: &mut [Suit]) -> (Suit, u32){
+
+    let min_range = 0;
+    let max_range = 52 * number_of_decks;
+    let card = rand::thread_rng().gen_range(min_range, max_range);
+   
+
+    let card: (Suit, u32)  = card_value(card);
+    
+    *player_cards_in_hand += card.1;
+    player_suits[*player_cards_in_hand as usize] = card.0;
+    *player_hand_value += card.1;
+
+
+    return card;
+
+}
+
+fn card_value(card_in: u32) -> (Suit, u32){
+    let card_in = card_in % 52; //Cut off the "deck number"
+    let suit = card_in % 13;
+    let value = card_in/13;
+    
+    let suit: Suit = match suit {
+        0 => Suit::Clubs, 
+        1 => Suit::Diamonds,
+        2 => Suit::Hearts,
+        3 => Suit::Spades,
+        _ => Suit::NoCard,
+    };
+
+    if let Suit::NoCard = suit
+    {
+        println!("Error: Card number is larger than 52 or lower than 0");
+    }
+
+    return (suit, value);
+}
